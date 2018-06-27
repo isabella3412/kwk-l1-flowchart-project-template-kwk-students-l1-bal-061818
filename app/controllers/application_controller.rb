a6e43c8b9be86eb5372a128b22fb26fd7fa26408
isabella3412 <izzybella17@icloud.com>
2018-06-27 20:15:34 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,16 @@
 class ApplicationController < Sinatra::Base
 
+  require 'bundler'
+  Bundler.require
+  require_relative 'models/questions'
+
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
 
+  get '/' do
+    erb :index
+  end
+#this file should contain my get routes (what we learned on tuesday w/ sinatra)
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,8 @@
+#this file should contain ruby
+#doesnt have to have class you can have a method.
+#this is just template
+
+
 class Question
 
 end
